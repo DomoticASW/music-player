3f053ce9182119980e9643088c49596530eb10c7
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2025-06-09 11:34:10 +0200

    refactor example to be cleaner

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -29,52 +29,61 @@ def emitEvent(e: Either[Event, Unit], ms: Int = 1000) =
       Thread.sleep(ms)
     case _ => ()
 
-def playMusic(): State[MusicState, Unit] =
+def pauseOrContinue(music: Music, t: Int, steps: Int): State[MusicState, Either[Event, Unit]] =
+  println(music.name + " is at " + t + "/" + music.duration)
+  val p = Random().nextDouble()
   for
-    e <- step(10)
+    e <- 
+      if p > 0.8
+      then pause()
+      else
+        Thread.sleep(1000)
+        play()
+    _ = emitEvent(e)
+    _ <- playMusic(steps)
+  yield e
+
+def changeMusicOrStopPlayer(music: Music, steps: Int): State[MusicState, Either[Event, Unit]] =
+  Thread.sleep(1000)
+  println(music.name + " just finished!\n")
+  Thread.sleep(500)
+  for
+    _ <- changeMusic(if music == backInBlack then dontStopBelievin else if music == dontStopBelievin then pokersFace else backInBlack)
+    p = Random().nextDouble()
+    e <-
+      if p > 0.9
+      then stop()
+      else play()
+    _ = emitEvent(e)
+    _ <- playMusic(steps)
+  yield e
+
+def restartingMusic(music: Music, steps: Int) =
+  println(music.name + " has beed paused")
+  println("Restarting in 3 seconds")
+  Thread.sleep(3000)
+  println(music.name + " is now restarting!")
+  Thread.sleep(500)
+  for
+    e <- play()
+    _ = emitEvent(e)
+    _ <- playMusic(steps)
+  yield e
+
+def playMusic(steps: Int): State[MusicState, Unit] =
+  for
+    e <- step(steps)
     _ = emitEvent(e)
     m <- currentState
     _ <- m match
       case Playing(music, t) =>
-        println(music.name + " is at " + t + "/" + music.duration)
-        val p = Random().nextDouble()
-        for
-          e <- 
-            if p > 0.8
-            then pause()
-            else
-              Thread.sleep(1000)
-              play()
-          _ = emitEvent(e)
-          _ <- playMusic()
-        yield e
+        pauseOrContinue(music, t, steps)
       case Paused(music, t) =>
         if t >= music.duration then
-          Thread.sleep(1000)
-          println(music.name + " just finished!\n")
-          Thread.sleep(500)
-          for
-            _ <- changeMusic(if music == backInBlack then dontStopBelievin else if music == dontStopBelievin then pokersFace else backInBlack)
-            p = Random().nextDouble()
-            e <-
-              if p > 0.9
-              then stop()
-              else play()
-            _ = emitEvent(e)
-            _ <- playMusic()
-          yield e
+          changeMusicOrStopPlayer(music, steps)
         else
-          println(music.name + " has beed paused")
-          println("Restarting in 3 seconds")
-          Thread.sleep(3000)
-          println(music.name + " is now restarting!")
-          Thread.sleep(500)
-          for
-            e <- play()
-            _ = emitEvent(e)
-            _ <- playMusic()
-          yield e
-      case _ => 
+          restartingMusic(music, steps)
+      case _ =>
         println("Music player powering off")
         stop()
   yield ()
@@ -92,7 +101,10 @@ def main =
   val state = for
     event <- changeMusic(backInBlack)
     _ = emitEvent(Left(event))
-    _ <- playMusic()
+    _ <- playMusic(steps = 10)
   yield ()
 
-  state.run(initialState)
+  val run: Runnable = () => state.run(initialState)
+  val musicPlayerThread = new Thread(run, "musicPlayer")
+  musicPlayerThread.start()
+  musicPlayerThread.join()
